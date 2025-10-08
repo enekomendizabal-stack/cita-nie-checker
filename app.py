c4a8e328d5f9b867b0828c1605ab69caf64bde6a
enekomendizabal-stack <eneko.mendizabal@opendeusto.es>
2025-10-08 18:21:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 
 app = Flask(__name__)
 
-URL = "https://sede.administracionespublicas.gob.es/icpplus/index"
+URL = "https://icp.administracionelectronica.gob.es/icpplus/acCitar"
 
 def comprobar():
     try:
